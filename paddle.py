ccccc9794bb3ff6c72f990450771d237c1e08107
laptopMSI <jesuisle@pcportable>
2025-02-04 14:48:13 +0100

    need to sort the different files

--- paddle.py
+++ paddle.py
@@ -3,7 +3,7 @@ class Paddle:
 		self.canvas = canvas
 		self.width = 10
 		self.height = 100
-		self.paddle = self.canvas.create_rectangle(x, y, x + self.width, y + self.height, fill=color)
+		self.paddle = self.canvas.create_rectangle(x - self.width/2, y - self.height/2, x + self.width/2, y + self.height/2, fill=color)
 
 	def move(self, dx, dy):
 		self.canvas.move(self.paddle, dx, dy)
@@ -12,4 +12,5 @@ class Paddle:
 		return self.canvas.coords(self.paddle)
 
 	def get_center(self):
-		return self.get_coords()[1] + self.height / 2
+		coords = self.get_coords()
+		return coords[0] + self.width / 2, coords[1] + self.height / 2
--- pong.py
+++ pong.py
@@ -1,45 +1,23 @@
-import random
-import numpy as np
 import tkinter as tk
 from ball import Ball
 from paddle import Paddle
-
-class Agent:
-	def __init__(self):
-		self.nbr_states = 3 # Above, Bellow, In front
-		self.nbr_actions = 3 # Up, Down, Stay
-		self.goal_state = 2
-
-		self.Q_table = np.zeros((self.nbr_states, self.nbr_actions))
-		self.learning_rate = 0.8 # alpha
-		self.discount_factor = 0.95 # gamma
-		self.exploration_prob = 0.2 # epsilon
-
-	def choose_action(self, current_state):
-		if np.random.rand() < self.exploration_prob:
-			return np.random.randint(0, self.nbr_actions)
-		else:
-			return np.argmax(self.Q_table[current_state])
-
-	def update(self, current_state, action, reward, next_state):
-		self.Q_table[current_state, action] += self.learning_rate * (reward + self.discount_factor * np.max(self.Q_table[next_state]) - self.Q_table[current_state, action])
+from math import *
+from agent import DQNAgent
+import numpy as np
 
 class Game:
-	def __init__(self, root):
+	def __init__(self, root, height, width, game_speed):
 		self.root = root
+		self.game_speed = game_speed
+		self.agent = DQNAgent(input_dim=5, output_dim=5)
 		self.root.title("Jeu Pong")
 
-		self.canvas = tk.Canvas(root, width=800, height=600, bg="black")
+		self.canvas = tk.Canvas(root, width=width, height=height, bg="black")
 		self.canvas.pack()
 
-		self.paddle_left = Paddle(self.canvas, 30, 250, color="red")
-		self.paddle_right = Paddle(self.canvas, 750, 250, color="blue")
-		self.ball = Ball(self.canvas, 390, 290, 20, "white", dx=random.randrange(-5, 6, 10), dy=0)
-		self.ia = Agent()
-
-		self.keys_pressed = {}
-		self.root.bind("<KeyPress>", self.key_press)
-		self.root.bind("<KeyRelease>", self.key_release)
+		self.paddle_left = Paddle(self.canvas, 50, height/2, color="red")
+		self.paddle_right = Paddle(self.canvas, width - 50, height/2, color="blue")
+		self.ball = Ball(self.canvas, width/2, height/2, 10, "white",  dx=0.95, dy=0.05, speed=5)
 
 		self.game_loop()
 
@@ -49,67 +27,94 @@ class Game:
 	def key_release(self, event):
 		self.keys_pressed[event.keysym] = False
 
-	def move_paddles(self, paddle, direction):
-		paddle_coords = paddle.get_coords()
-		if paddle_coords[1] - 10 >= 0 and direction == "Up":
-			paddle.move(0, -10)
-		if paddle_coords[3] + 10 <= 600 and direction == "Down":
-			paddle.move(0, 10)
-
-	def game_loop(self):
-		self.Qlearning(self.paddle_left)
-		self.Qlearning(self.paddle_right)
-		victory = self.ball.check_collision_with_wall()
-		if victory != 0:
-			self.ball.reset()
-		if self.ball.dx > 0:
-			self.ball.check_collision_with_paddle(self.paddle_right)
-		else:
-			self.ball.check_collision_with_paddle(self.paddle_left)
-
-		self.root.after(20, self.game_loop)
-
-	def get_paddle_state(self, paddle):
+	def check_collision_with_paddle(self, ball, paddle):
+		paddle_center = paddle.get_center()
+		ball_center = self.ball.get_center()
 		ball_coords = self.ball.get_coords()
 		paddle_coords = paddle.get_coords()
 		bx1, by1, bx2, by2 = ball_coords
 		px1, py1, px2, py2 = paddle_coords
-		if by1 > py1:
-			return 0
-		elif by2 < py2:
-			return 1
-		else:
-			return 2
 
-	def get_reward(self, state, action):
-		if state == 0 and action == 1:
-			return 1
-		if state == 1 and action == 0:
-			return 1
-		if state == 2 and action == 2:
-			return 1
-		return -1
+		if bx2 >= px1 and bx1 <= px2 and by2 >= py1 and by1 <= py2:
+			angle = (ball_center[1] - paddle_center[1]) / 50
+			if ball.dx < 0:
+				ball.dx = 1 - abs(angle)
+			else:
+				ball.dx = -(1 - abs(angle))
+			ball.dy = angle
 
-	def Qlearning(self, paddle):
-		# Choose action
-		current_state = self.get_paddle_state(paddle)
-		action = self.ia.choose_action(current_state)
+	def check_collision_with_wall(self, ball):
+		"""return zero if no victory, 1 for RED and 2 for BLUE"""
+		ball_coords = ball.get_coords()
+		bx1, by1, bx2, by2 = ball_coords
 
-		# Execute action
-		if action == 0:
-			self.move_paddles(paddle, "Up")
-		elif action == 1:
+		if by1 <= 0 or by2 >= self.canvas.winfo_height():
+			ball.dy = -ball.dy
+		if bx1 <= 0:
+			ball.dx = -ball.dx
+			print("BLUE WIN")
+			ball.reset()
+		if bx2 >= 800:
+			ball.dx = -ball.dx
+			print("RED WIN")
+			ball.reset()
+
+	def move_paddles(self, paddle, direction):
+		paddle_coords = paddle.get_coords()
+		if paddle_coords[1] - 10 >= 0 and direction == "Up":
+			paddle.move(0, -10)
+		elif paddle_coords[3] + 10 <= 600 and direction == "Down":
+			paddle.move(0, 10)
+
+	def simpleOpponent(self, paddle):
+		ball_c = self.ball.get_center()
+		paddle_c = paddle.get_center()
+		if ball_c[1] > paddle_c[1]:
 			self.move_paddles(paddle, "Down")
+		elif ball_c[1] < paddle_c[1]:
+			self.move_paddles(paddle, "Up")
 
-		# Get a reward
-		reward = self.get_reward(current_state, action)
+	def get_reward(self):
+		ball_coords = self.ball.get_coords()
+		paddle_coords = self.paddle_right.get_coords()
+		bx1, by1, bx2, by2 = ball_coords
+		px1, py1, px2, py2 = paddle_coords
 
-		# Update Q-table
-		current_state = self.get_paddle_state(paddle)
-		self.ia.update(current_state, action, reward, current_state)
-		self.ia.exploration_prob /= 2
+		if bx2 >= px1 and bx1 <= px2 and by2 >= py1 and by1 <= py2:
+			reward += 1
+		if bx2 >= px2:
+			reward -= 1
+		return reward
+
+	def DQN(self, state):
+		# Exploitation vs exploration
+		action = self.agent.choose_action(state)
+
+		if action == 0:
+			self.move_paddles(self.paddle_right, "Up")
+		elif action == 1:
+			self.move_paddles(self.paddle_right, "Down")
+
+		# Calculate reward
+		reward = self.agent.get_reward(state)
+		done = self.is_done(state)
+
+		next_state = np.array([[self.ball.get_center[0], self.ball.get_center[1], self.ball.vx, self.ball.vy, self.paddle_right.get_center()[1]]])
+		self.agent.update(state, action, reward, next_state, done)
+
+		self.agent.decrease_exploration_prob()
+
+	def game_loop(self):
+		self.simpleOpponent(self.paddle_left)
+		state = np.array([[self.ball.get_center[0], self.ball.get_center[1], self.ball.vx, self.ball.vy, self.paddle_right.get_center()[1]]])
+		self.agent.DQN(state)
+		self.ball.move()
+		self.check_collision_with_paddle(self.ball, self.paddle_left)
+		self.check_collision_with_paddle(self.ball, self.paddle_right)
+		self.check_collision_with_wall(self.ball)
+		self.root.after((int)(20 / self.game_speed), self.game_loop)
 
 if __name__ == "__main__":
 	root = tk.Tk()
-	game = Game(root)
+	game = Game(root, 600, 800, 1)
 	root.mainloop()
